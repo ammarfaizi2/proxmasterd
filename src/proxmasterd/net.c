b34ff95bff6f5e901887498188a89633d34bf399
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-09-28 21:48:16 +0700

    @must_rebase: Clean up net subsystem
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- src/proxmasterd/net.c
+++ src/proxmasterd/net.c
@@ -2,16 +2,21 @@
 
 #include <proxmasterd/net.h>
 
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 enum {
-	EPL_EVT_EVENTFD		= (1ull << 48ull),
-	EPL_EVT_CLIENT_POLLIN	= (2ull << 48ull),
-	EPL_EVT_CLIENT_POLLOUT	= (3ull << 48ull),
-	EPL_EVT_ACCEPT		= (4ull << 48ull),
+	EPL_EVT_EVENTFD	= (1ull << 48ull),
+	EPL_EVT_CLIENT	= (2ull << 48ull),
+	EPL_EVT_ACCEPT	= (3ull << 48ull),
 };
+
+#define EPL_EV_MASK		(0xffffull << 48ull)
+#define GET_EPL_EV(data)	((data) & EPL_EV_MASK)
+#define GET_EPL_DT(data)	((void *)((data) & ~EPL_EV_MASK))
+#define INIT_BUF_SIZE		4096
 
 int pm_stack_u32_init(struct pm_stack_u32 *s, size_t cap)
 {
@@ -145,9 +150,12 @@ int pm_buf_append(struct pm_buf *b, const void *data, size_t len)
 {
 	size_t new_len;
 
-	new_len = b->len + len + 1;
+	if (!len)
+		return 0;
+
+	new_len = b->len + len;
 	if (new_len > b->cap) {
-		if (pm_buf_resize(b, new_len * 2))
+		if (pm_buf_resize(b, (new_len + 1) * 2))
 			return -ENOMEM;
 	}
 
@@ -260,6 +268,8 @@ static int clients_init(struct pm_net_wrk *w)
 			return -ENOMEM;
 		}
 
+		c->fd = -1;
+		c->idx = i;
 		clients[i] = c;
 		__pm_stack_u32_push(&w->stack, i);
 	}
@@ -282,6 +292,30 @@ static int epoll_mod(int ep_fd, int fd, uint32_t events, union epoll_data data)
 {
 	struct epoll_event ev = { .events = events, .data = data };
 	return epoll_ctl(ep_fd, EPOLL_CTL_MOD, fd, &ev);
+}
+
+static int send_event_fd(struct pm_net_wrk *w)
+{
+	uint64_t val = 1;
+	ssize_t ret;
+
+	ret = write(w->ev_fd, &val, sizeof(val));
+	if (ret < 0)
+		return -errno;
+
+	return 0;
+}
+
+static int recv_event_fd(struct pm_net_wrk *w)
+{
+	uint64_t val;
+	ssize_t ret;
+
+	ret = read(w->ev_fd, &val, sizeof(val));
+	if (ret < 0)
+		return -errno;
+
+	return 0;
 }
 
 static int epoll_init(struct pm_net_wrk *w)
@@ -360,6 +394,18 @@ static int worker_init(struct pm_net_wrk *w)
 		return ret;
 	}
 
+	if (w->idx == 0) {
+		union epoll_data data;
+
+		data.u64 = EPL_EVT_ACCEPT;
+		ret = epoll_add(w->ep_fd, w->ctx->tcp_fd, EPOLLIN, data);
+		if (ret) {
+			epoll_destroy(w);
+			clients_destroy(w);
+			return ret;
+		}
+	}
+
 	ret = pthread_create(&w->thread, NULL, &worker_entry, w);
 	if (ret) {
 		epoll_destroy(w);
@@ -431,10 +477,375 @@ static int workers_init(struct pm_net_ctx *ctx)
 	return 0;
 }
 
-enum {
-	WORKER_WAIT_RUN  = 0,
-	WORKER_WAIT_STOP = 1,
+static int get_client_slot(struct pm_net_wrk *w, struct pm_net_client **cp)
+{
+	struct pm_net_client *c;
+	uint32_t idx;
+	int ret;
+
+	pthread_mutex_lock(&w->stack.lock);
+	ret = __pm_stack_u32_pop(&w->stack, &idx);
+	if (ret) {
+		pthread_mutex_unlock(&w->stack.lock);
+		return -EAGAIN;
+	}
+	c = w->clients[idx];
+	pthread_mutex_unlock(&w->stack.lock);
+
+	assert(c);
+	assert(c->fd < 0);
+	assert(!c->recv_buf.len);
+	assert(!c->send_buf.len);
+	*cp = c;
+	atomic_fetch_add(&w->nr_online_conn, 1u);
+	return ret;
+}
+
+static int __put_client_slot(struct pm_net_wrk *w, struct pm_net_client *c, bool del_epoll)
+{
+	int ret;
+
+	pthread_mutex_lock(&w->stack.lock);
+	if (c->fd >= 0) {
+		if (del_epoll) {
+			ret = epoll_del(w->ep_fd, c->fd);
+			assert(!ret);
+		}
+		close(c->fd);
+		c->fd = -1;
+	}
+
+	if (c->recv_buf.cap > INIT_BUF_SIZE)
+		pm_buf_resize(&c->recv_buf, INIT_BUF_SIZE);
+	if (c->send_buf.cap > INIT_BUF_SIZE)
+		pm_buf_resize(&c->send_buf, INIT_BUF_SIZE);
+
+	c->recv_cb = NULL;
+	c->send_cb = NULL;
+	ret = __pm_stack_u32_push(&w->stack, w->idx);
+	assert(!ret);
+	pthread_mutex_unlock(&w->stack.lock);
+	atomic_fetch_sub(&w->nr_online_conn, 1u);
+	return ret;
+}
+
+static int put_client_slot(struct pm_net_wrk *w, struct pm_net_client *c)
+{
+	return __put_client_slot(w, c, true);
+}
+
+static int put_client_slot_no_epoll(struct pm_net_wrk *w, struct pm_net_client *c)
+{
+	return __put_client_slot(w, c, false);
+}
+
+static struct pm_net_wrk *pick_worker_for_new_conn(struct pm_net_ctx *ctx)
+{
+	struct pm_net_wrk *w = &ctx->workers[0];
+	uint32_t i, min, min_idx = 0, tmp;
+
+	if (ctx->arg.nr_workers == 1)
+		return w;
+
+	min = atomic_load_explicit(&w->nr_online_conn, memory_order_relaxed);
+	for (i = 1; i < ctx->arg.nr_workers; i++) {
+		w = &ctx->workers[i];
+		tmp = atomic_load_explicit(&w->nr_online_conn, memory_order_relaxed);
+
+		if (tmp < min) {
+			min = tmp;
+			min_idx = i;
+		}
+	}
+
+	return &ctx->workers[min_idx];
+}
+
+static int handle_accept_error(int err, struct pm_net_wrk *w)
+{
+	if (err == EAGAIN || err == EINTR)
+		return 0;
+
+	if (err == EMFILE || err == ENFILE) {
+		pthread_mutex_lock(&w->ctx->accept_mutex);
+		w->ctx->accept_stopped = true;
+		pthread_mutex_unlock(&w->ctx->accept_mutex);
+		return epoll_del(w->ep_fd, w->ctx->tcp_fd);
+	}
+
+	return -err;
+}
+
+/*
+ * @fd: The ownership is taken by give_client_fd_to_a_worker().
+ */
+static int give_client_fd_to_a_worker(struct pm_net_ctx *ctx, int fd,
+				      const struct sockaddr_in46 *addr)
+{	
+	struct pm_net_client *c;
+	struct pm_net_wrk *w;
+	union epoll_data data;
+	int r;
+
+	w = pick_worker_for_new_conn(ctx);
+	r = get_client_slot(w, &c);
+	if (r) {
+		close(fd);
+		return -ENOMEM;
+	}
+
+	c->fd = fd;
+	c->src_addr = *addr;
+	c->ep_mask = EPOLLIN;
+
+	data.u64 = 0;
+	data.ptr = c;
+	data.u64 |= EPL_EVT_CLIENT;
+	r = epoll_add(w->ep_fd, fd, c->ep_mask, data);
+	if (r) {
+		put_client_slot_no_epoll(w, c);
+		return r;
+	}
+
+	return 0;
+}
+
+static int handle_event_accept(struct pm_net_wrk *w)
+{
+	static const uint32_t NR_MAX_ACCEPT_CYCLE = 4;
+	struct sockaddr_in46 addr;
+	uint32_t counter = 0;
+	socklen_t len;
+	int ret;
+
+do_accept:
+	memset(&addr, 0, sizeof(addr));
+	if (w->ctx->arg.bind_addr.sa.sa_family == AF_INET)
+		len = sizeof(struct sockaddr_in);
+	else
+		len = sizeof(struct sockaddr_in6);
+
+	ret = accept4(w->ctx->tcp_fd, &addr.sa, &len, SOCK_NONBLOCK | SOCK_CLOEXEC);
+	if (ret < 0)
+		return handle_accept_error(errno, w);
+
+	if (len > sizeof(addr)) {
+		close(ret);
+		return -EINVAL;
+	}
+
+	ret = give_client_fd_to_a_worker(w->ctx, ret, &addr);
+	if (ret)
+		return 0;
+
+	if (++counter < NR_MAX_ACCEPT_CYCLE)
+		goto do_accept;
+
+	return 0;
+}
+
+static int apply_ep_mask(struct pm_net_wrk *w, struct pm_net_client *c)
+{
+	union epoll_data data;
+
+	data.u64 = 0;
+	data.ptr = c;
+	data.u64 |= EPL_EVT_CLIENT;
+	return epoll_mod(w->ep_fd, c->fd, c->ep_mask, data);
+}
+
+static int handle_event_client_send(struct pm_net_wrk *w,
+				    struct pm_net_client *c)
+{
+	struct pm_buf *b = &c->send_buf;
+	ssize_t ret;
+	size_t len;
+	int err;
+
+	len = b->len;
+	if (!len)
+		return 0;
+
+	err = 0;
+	ret = send(c->fd, b->buf, len, MSG_DONTWAIT);
+	if (ret > 0) {
+		memmove(b->buf, b->buf + ret, len - ret);
+		b->len -= (size_t)ret;
+		if (c->send_cb)
+			err = c->send_cb(c, ret);
+	} else if (ret == 0) {
+		err = -ECONNRESET;
+	} else {
+		err = -errno;
+	}
+
+	if (err == -EAGAIN || err == -EINTR)
+		err = 0;
+
+	if (!err) {
+		if (b->len) {
+			if (!(c->ep_mask & EPOLLOUT)) {
+				c->ep_mask |= EPOLLOUT;
+				err = apply_ep_mask(w, c);
+			}
+		} else if (c->ep_mask & EPOLLOUT) {
+			c->ep_mask &= ~EPOLLOUT;
+			err = apply_ep_mask(w, c);
+		}
+	}
+
+	return err;
+}
+
+static int handle_event_client_recv(struct pm_net_wrk *w,
+				    struct pm_net_client *c)
+{
+	struct pm_buf *b = &c->recv_buf;
+	ssize_t ret;
+	size_t len;
+	int err;
+
+	len = b->cap - b->len;
+	if (!len) {
+		if (pm_buf_resize(b, (b->cap + 1) * 2))
+			return -ENOMEM;
+		len = b->cap - b->len;
+	}
+
+	err = 0;
+	ret = recv(c->fd, b->buf + b->len, len, MSG_DONTWAIT);
+	if (ret > 0) {
+		b->len += (size_t)ret;
+		if (c->recv_cb)
+			err = c->recv_cb(c, ret);
+	} else if (ret == 0) {
+		err = -ECONNRESET;
+	} else {
+		err = -errno;
+	}
+
+	if (err == -EAGAIN || err == -EINTR)
+		err = 0;
+
+	if (!err && b->len)
+		err = handle_event_client_send(w, c);
+
+	return err;
+}
+
+static int handle_event_client(struct pm_net_wrk *w, struct epoll_event *ev)
+{
+	struct pm_net_client *c = GET_EPL_DT(ev->data.u64);
+	uint32_t events = ev->events;
+	int ret = 0;
+
+	if (events & EPOLLIN) {
+		ret = handle_event_client_recv(w, c);
+		if (ret)
+			return ret;
+	}
+
+	if (events & EPOLLOUT) {
+		ret = handle_event_client_send(w, c);
+		if (ret)
+			return ret;
+	}
+
+	if (events & (EPOLLERR | EPOLLHUP))
+		ret = -ECONNRESET;
+
+	return ret;
+}
+
+/*
+ * Accept and event FD are low priority events.
+ * Handle them after all other events.
+ */
+struct epl_handle_ev {
+	bool	has_event_accept;
+	bool	has_event_evfd;
 };
+
+static int handle_event(struct pm_net_wrk *w, struct epoll_event *ev,
+			struct epl_handle_ev *he)
+{
+	uint64_t ev_type = GET_EPL_EV(ev->data.u64);
+	int ret = 0;
+
+	switch (ev_type) {
+	case EPL_EVT_CLIENT:
+		ret = handle_event_client(w, ev);
+		if (ret) {
+			struct pm_net_client *c = GET_EPL_DT(ev->data.u64);
+
+			put_client_slot(w, c);
+			ret = 0;
+		}
+		break;
+	case EPL_EVT_ACCEPT:
+		he->has_event_accept = true;
+		break;
+	case EPL_EVT_EVENTFD:
+		he->has_event_evfd = true;
+		break;
+	default:
+		break;
+	}
+
+
+	return ret;
+}
+
+static int handle_low_priority_events(struct pm_net_wrk *w,
+				      struct epl_handle_ev *he)
+{
+	struct pm_net_ctx *ctx = w->ctx;
+	int ret = 0;
+
+	if (ctx->should_stop)
+		return ret;
+
+	if (he->has_event_evfd) {
+		ret = recv_event_fd(w);
+		if (ret)
+			return ret;
+	}
+
+	if (he->has_event_accept) {
+		ret = handle_event_accept(w);
+		if (ret)
+			return ret;
+	}
+
+	return ret;
+}
+
+static int handle_events(struct pm_net_wrk *w, int nr_events)
+{
+	struct pm_net_ctx *ctx = w->ctx;
+	struct epl_handle_ev he;
+	int ret = 0, i;
+
+	if (!nr_events)
+		return 0;
+
+	memset(&he, 0, sizeof(he));
+	for (i = 0; i < nr_events; i++) {
+		struct epoll_event *ev = &w->events[i];
+
+		ret = handle_event(w, ev, &he);
+		if (ret < 0)
+			break;
+
+		if (ctx->should_stop)
+			break;
+	}
+
+	if (!ret)
+		ret = handle_low_priority_events(w, &he);
+
+	return ret;
+}
 
 static int poll_events(struct pm_net_wrk *w)
 {
@@ -447,12 +858,15 @@ static int poll_events(struct pm_net_wrk *w)
 		ret = -errno;
 		if (ret == -EINTR)
 			return 0;
-
-		return ret;
 	}
 
 	return ret;
 }
+
+enum {
+	WORKER_WAIT_RUN  = 0,
+	WORKER_WAIT_STOP = 1,
+};
 
 static int worker_wait_for_start_signal(struct pm_net_wrk *w)
 {
@@ -480,17 +894,25 @@ static int worker_wait_for_start_signal(struct pm_net_wrk *w)
 static void *worker_entry(void *arg)
 {
 	struct pm_net_wrk *w = arg;
+	struct pm_net_ctx *ctx = w->ctx;
 	int ret;
 
 	ret = worker_wait_for_start_signal(w);
 	if (ret == WORKER_WAIT_STOP)
 		goto out;
 
+	while (!ctx->should_stop) {
+		ret = poll_events(w);
+		if (ret < 0)
+			break;
 
-
+		ret = handle_events(w, ret);
+		if (ret < 0)
+			break;
+	}
 
 out:
-	pm_net_ctx_stop(w->ctx);
+	pm_net_ctx_stop(ctx);
 	return NULL;
 }
 
@@ -505,8 +927,15 @@ int pm_net_ctx_init(struct pm_net_ctx *ctx, const struct pm_net_ctx_arg *arg)
 	if (ret)
 		return ret;
 
+	ret = pthread_mutex_init(&ctx->accept_mutex, NULL);
+	if (ret) {
+		pthread_mutex_destroy(&ctx->start_lock);
+		return ret;
+	}
+
 	ret = pthread_cond_init(&ctx->start_cond, NULL);
 	if (ret) {
+		pthread_mutex_destroy(&ctx->accept_mutex);
 		pthread_mutex_destroy(&ctx->start_lock);
 		return ret;
 	}
@@ -515,6 +944,7 @@ int pm_net_ctx_init(struct pm_net_ctx *ctx, const struct pm_net_ctx_arg *arg)
 	if (ret) {
 		pthread_cond_destroy(&ctx->start_cond);
 		pthread_mutex_destroy(&ctx->start_lock);
+		pthread_mutex_destroy(&ctx->accept_mutex);
 		return ret;
 	}
 
@@ -523,6 +953,7 @@ int pm_net_ctx_init(struct pm_net_ctx *ctx, const struct pm_net_ctx_arg *arg)
 		sock_destroy(ctx);
 		pthread_cond_destroy(&ctx->start_cond);
 		pthread_mutex_destroy(&ctx->start_lock);
+		pthread_mutex_destroy(&ctx->accept_mutex);
 		return ret;
 	}
 
@@ -537,11 +968,27 @@ void pm_net_ctx_run(struct pm_net_ctx *ctx)
 	pthread_mutex_unlock(&ctx->start_lock);
 }
 
+void pm_net_ctx_wait(struct pm_net_ctx *ctx)
+{
+	pthread_mutex_lock(&ctx->start_lock);
+	while (!ctx->should_stop)
+		pthread_cond_wait(&ctx->start_cond, &ctx->start_lock);
+	pthread_mutex_unlock(&ctx->start_lock);
+}
+
 void pm_net_ctx_stop(struct pm_net_ctx *ctx)
 {
+	uint32_t i;
+
 	pthread_mutex_lock(&ctx->start_lock);
 	ctx->should_stop = true;
 	pthread_cond_broadcast(&ctx->start_cond);
+	for (i = 0; i < ctx->arg.nr_workers; i++) {
+		struct pm_net_wrk *w = &ctx->workers[i];
+
+		if (w->need_join_thread)
+			send_event_fd(w);
+	}
 	pthread_mutex_unlock(&ctx->start_lock);
 }
 
@@ -552,4 +999,6 @@ void pm_net_ctx_destroy(struct pm_net_ctx *ctx)
 	sock_destroy(ctx);
 	pthread_cond_destroy(&ctx->start_cond);
 	pthread_mutex_destroy(&ctx->start_lock);
+	pthread_mutex_destroy(&ctx->accept_mutex);
+	memset(ctx, 0, sizeof(*ctx));
 }
--- src/proxmasterd/net.h
+++ src/proxmasterd/net.h
@@ -31,9 +31,14 @@ struct pm_buf {
 
 struct pm_net_client {
 	int			fd;
+	uint32_t		idx;
+	uint32_t		ep_mask;
 	struct pm_buf		recv_buf;
 	struct pm_buf		send_buf;
 	struct sockaddr_in46	src_addr;
+
+	int (*recv_cb)(struct pm_net_client *c, ssize_t recv_ret);
+	int (*send_cb)(struct pm_net_client *c, ssize_t send_ret);
 };
 
 struct pm_stack_u32 {
@@ -50,14 +55,15 @@ struct pm_net_wrk {
 	int			ev_fd;
 	uint32_t		idx;
 	uint32_t		nr_events;
+	_Atomic(uint32_t)	nr_online_conn;
 	struct epoll_event	*events;
 	struct pm_net_ctx	*ctx;
 	struct pm_net_client	**clients;
 	struct pm_stack_u32	stack;
 	size_t			client_cap;
-	size_t			nr_online_conn;
 	pthread_t		thread;
 	volatile bool		need_join_thread;
+	volatile bool		handle_event_should_break;
 };
 
 struct pm_net_ctx_arg {
@@ -70,6 +76,8 @@ struct pm_net_ctx_arg {
 struct pm_net_ctx {
 	volatile bool		should_stop;
 	volatile bool		started;
+	volatile bool		accept_stopped;
+	pthread_mutex_t		accept_mutex;
 	pthread_mutex_t		start_lock;
 	pthread_cond_t		start_cond;
 	int			tcp_fd;
@@ -77,12 +85,19 @@ struct pm_net_ctx {
 	struct pm_net_ctx_arg	arg;
 };
 
+int __pm_stack_u32_push(struct pm_stack_u32 *s, uint32_t v);
+int pm_stack_u32_push(struct pm_stack_u32 *s, uint32_t v);
+int __pm_stack_u32_pop(struct pm_stack_u32 *s, uint32_t *v);
+int pm_stack_u32_pop(struct pm_stack_u32 *s, uint32_t *v);
+
 int pm_buf_init(struct pm_buf *b, size_t cap);
 int pm_buf_append(struct pm_buf *b, const void *data, size_t len);
 int pm_buf_resize(struct pm_buf *b, size_t new_cap);
 void pm_buf_destroy(struct pm_buf *b);
 
+int pm_net_ctx_init(struct pm_net_ctx *ctx, const struct pm_net_ctx_arg *arg);
 void pm_net_ctx_run(struct pm_net_ctx *ctx);
+void pm_net_ctx_wait(struct pm_net_ctx *ctx);
 void pm_net_ctx_stop(struct pm_net_ctx *ctx);
 void pm_net_ctx_destroy(struct pm_net_ctx *ctx);
 
