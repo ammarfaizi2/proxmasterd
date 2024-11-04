076e96cd4089fd89ff8c03f594d8f7600fd14987
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-11-05 00:51:04 +0700

    Clean up speedmgr
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- speedmgr/speedmgr.c
+++ speedmgr/speedmgr.c
@@ -1,8 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0-only
-
 /*
  *
- * Copyright (C) 2024  Ammar Faizi <ammarfaizi2@gnuweeb.org>
+ * Ammar Faizi <ammarfaizi2@gnuweeb.org>
  *
  * speedmgr - A simple speed manager for Linux.
  * 
